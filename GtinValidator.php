49fc8f8415d64a168257253ba5f9335b0febe368
Kankro <erkzer0@hotmail.it>
2021-03-12 14:14:36 +0100

    Update GtinValidator.php
    Add check prefix for validate gtin.

--- GtinValidator.php
+++ GtinValidator.php
@@ -2,6 +2,19 @@
 
 class GtinValidator
 {
+    // List simple prefix to exclude https://www.gs1.org/standards/id-keys/company-prefix
+    public static $gtinPrefixList = [
+        [200,299], // Used to issue GS1 Restricted Circulation Numbers within a geographic region (MO defined)
+        951, // Global Office - General Manager Number, see Note 2
+        952, // Used for demonstrations and examples of the GS1 system
+        [960,969], // Global Office - GTIN-8, see note 3
+        977, // Serial publications (ISSN)
+        [978,979], // Bookland (ISBN)
+        980, // Refund receipts
+        [981,984], // GS1 coupon identification for common currency areas
+        99, //GS1 coupon identification
+    ];
+
     public function isValidGtin($code)
     {
         // Validates any GTIN-8, GTIN-12, GTIN-13 or GTIN-14 code.
@@ -31,10 +44,36 @@ class GtinValidator
 
     protected function isValidCode($code)
     {
+        if (!$this->checkPrefixValidation($code)) {
+            return false;
+        }
         if (!is_numeric($code) || !in_array(strlen($code), [8,12,13,14])) {
             return false;
         }
         return $this->isGtinChecksumValid(str_pad($code, 14, "0", STR_PAD_LEFT));
+    }
+
+    protected function checkPrefixValidation($code)
+    {
+        $code3 = (int)substr($code, 0, 3);
+        $code2 = (int)substr($code, 0, 2);
+        $flag = true;
+        foreach (static::$gtinPrefixList as $gtinList) {
+            if (is_array($gtinList)) {
+                if ($gtinList[0] <= $code3 && $code3 <= $gtinList[1]) {
+                    $flag = false;
+                }
+            } elseif (strlen($gtinList) == 3) {
+                if ($code3 == $gtinList) {
+                    $flag = false;
+                }
+            } else {
+                if ($code2 == $gtinList) {
+                    $flag = false;
+                }
+            }
+        }
+        return $flag;
     }
 
     protected function gtinChecksum($code)
